059ac7e7ca573c066688b084ad08ec68d7a41228
Alex Choi <rlrrlrll4@gmail.com>
2013-09-21 04:36:27 -0500

    works for non-nested

--- if_block.js
+++ if_block.js
@@ -1,38 +1,27 @@
 //output: array of lines without if statement
 //make new array_lines, but don't add the if statements, making the new array_lines concise
 //ONLY add the if statements whose conditions are true, without the if(){}
-function if_block(array_lines){
-	var start_index;
-	var end_index;
-	var exclude_start;
-	var exclude_end;
+function if_block(array_lines,vstorage){
+	var start_index =[];
+	var end_index =[];
+	var exclude_start =[];
+	var exclude_end =[];
 	var temp1=0;
 	var statement;
-	var return_lines;
+	var return_lines =[];
 
-/*	var variableStorage ={};
-	variableStorage["x"] = 5;
-	variableStorage["y"] = 6;
-
-	(first in variableStorage)  true
-
-	eval(variableStorage["x"]+6); */
-
-
-
-
-	for(int a=0; a<array_lines.length; a++){
-		for(int b=0; b<array_lines[a].length; b++){
+	for(var a=0; a<array_lines.length; a++){
+		for(var b=0; b<array_lines[a].length; b++){
 			if(array_lines[a][b]==="if"){
 				start_index.push(a);
 			}
 		}
 	}
 
-	for(int a=0; a<start_index.length; a++){
+	for(var a=0; a<start_index.length; a++){
 		temp1=1; //starts at 1 because if line contains open bracket
-		for(int b=start_index[a]+1; start_index.length; b++){
-			for(int c=0; c<array_lines[b].length; c++){
+		for(var b=start_index[a]+1; b<start_index.length; b++){
+			for(var c=0; c<(array_lines[b]).length; c++){
 				if(array_lines[b][c]==="{"){
 					temp1++;
 				}else if(array_lines[b][c]==="}"){
@@ -45,13 +34,13 @@ function if_block(array_lines){
 		}
 	}
 
-	for(int a=0; a<start_index.length; a++){
+	for(var a=0; a<start_index.length; a++){
 		temp1 = start_index[a];
 		statement = "";
 		
-		for(int b=2; array_lines[temp1][b]!==")"; b++){ //b=2 because first two elements are "if" and "("
-			if(isNaN(array_lines[temp1][b])){
-				eval(array_lines[temp1][b] + "=" + vstorage[array_lines[temp1][b]].toString());
+		for(var b=2; array_lines[temp1][b]!==")"; b++){ //b=2 because first two elements are "if" and "("
+			if(isNaN(array_lines[temp1][b]) && isValid(array_lines[temp1][b])){
+				eval(array_lines[temp1][b] +"="+ vstorage[array_lines[temp1][b]].toString()); // vstorage[array_lines[temp1][b]].toString());
 			}
 			statement = statement + array_lines[temp1][b];
 		}
@@ -62,7 +51,7 @@ function if_block(array_lines){
 		}
 	}
 
-	for(int a=0; a<array_lines.length; a++){
+	for(var a=0; a<array_lines.length; a++){
 		if(a==start_index[0]){
 			if(a==exclude_start[0]){
 				a = exclude_end[0];
@@ -79,5 +68,3 @@ function if_block(array_lines){
 
 	return return_lines;
 }
-
-
