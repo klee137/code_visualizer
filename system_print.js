609ac38138cd3c91e1802ad9cb1666ea61509092
E L <psybuzz@gmail.com>
2013-09-24 21:47:23 -0500

    mhacks version is in tester.html

--- system_print.js
+++ system_print.js
@@ -5,14 +5,14 @@ function system_print(array_lines, vstorage){
 
 	for(var a=0; a<array_lines.length; a++){
 		for(var b=0; b<array_lines.length; b++){
-			if(array_lines[a][b]==="System" && array_lines[a][b+1]==="." && array_lines[a][b+2]==="out" && (array_lines[a][b+3]==="println" || array_lines[a][b+3]==="print"){
+			if(array_lines[a][b]==="System" && array_lines[a][b+1]==="." && array_lines[a][b+2]==="out" && array_lines[a][b+3]==="println" || array_lines[a][b+3]==="print"){
 				if(array_lines[a][b+3]==="println"){
 					println = 1;
 				}else{
 					println = 0;
 				}
 				b=b+5;
-				while(array_lines[a][b!=")"){
+				while(array_lines[a][b]!=")"){
 					statement = statement + " "+ array_lines[a][b];
 				}
 			}
